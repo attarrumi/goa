5b586e5b75cd6b939f2f7801f19804b60925caea
walid <attar4444@gmail.com>
2023-03-16 07:04:51 +0700

    unsafe link

--- unsafe_asm.go
+++ unsafe_asm.go
@@ -2,13 +2,13 @@ package goa
 
 import "unsafe"
 
-//go:linkname clang_one clang_one
+//go:linkname unsafe_one unsafe_one
 func unsafe_one(fn unsafe.Pointer, x unsafe.Pointer) unsafe.Pointer
 
-//go:linkname clang_two clang_two
+//go:linkname unsafe_two unsafe_two
 func unsafe_two(fn unsafe.Pointer, x, y unsafe.Pointer) unsafe.Pointer
 
-//go:linkname clang_three clang_three
+//go:linkname unsafe_three unsafe_three
 func unsafe_three(fn unsafe.Pointer, x unsafe.Pointer, y unsafe.Pointer, z unsafe.Pointer) unsafe.Pointer
 
 func UnsafeOne(fn unsafe.Pointer, x unsafe.Pointer) unsafe.Pointer {
